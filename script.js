9afd4f7edfc4fad954b41eb949672acab5206e09
DrBillys <DrBillys16@gmail.com>
2026-09-24 12:39:49 +0300

    Add alert for testing in downloadPdf function

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
 function downloadPdf() {
-
-    const firstName = document.getElementById("firstname").value;
-    const lastName = document.getElementById("lastname").value;
-    const company = document.getElementById("company").value;
-    const email = document.getElementById("email").value;
-
-    if (!firstName || !lastName || !company || !email) {
-        alert("Please complete all fields.");
-        return;
-    }
-
-    const formData = new FormData();
-
-    formData.append("entry.575272416", firstName);
-    formData.append("entry.1950660787", lastName);
-    formData.append("entry.952978449", company);
-    formData.append("entry.1269496819", email);
-
-    fetch(
-        "https://docs.google.com/forms/d/e/1FAIpQLSdm8ZoiewAj4hRJ807AOkLm3sTmuyKQdlcYxrzMRztlyCwbDg/formResponse",
-        {
-            method: "POST",
-            mode: "no-cors",
-            body: formData
-        }
-    );
-
-    const link = document.createElement("a");
-
-    link.href = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN ONEPAGER ENG.pdf";
-    link.download = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN ONEPAGER ENG.pdf";
-
-    document.body.appendChild(link);
-    link.click();
-
-    document.body.removeChild(link);
+    alert("TEST");
 }
